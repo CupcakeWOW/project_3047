cf269e60d2bf19bb035708e7dcba53c4329770c1
CupcakeWOW <chiwai737@gmail.com>
2018-10-13 18:49:12 +0900

    first upload

---
+++ api/models/Person.js
@@ -0,0 +1,77 @@
+/**
+ * Person.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    name: {
+      type: "string"
+    },
+
+
+    short_des: {
+      type: "string"
+    },
+
+    full_des: {
+      type: "string"
+    },
+
+    img: {
+      type: "string"
+    },
+
+    org: {
+      type: "string"
+    },
+
+    date: {
+      type: "string"
+    },
+
+    time: {
+      type: "string"
+    },
+
+    venue: {
+      type: "string"
+    },
+
+    quota: {
+      type: "number"
+    },
+
+    highlight: {
+      type: "string"
+    }
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+  getInvalidIdMsg: function (opts) {
+
+    if (typeof opts.id === "undefined" || isNaN(parseInt(opts.id)))
+      return "Person id not specified or with incorrect type.";
+
+    return null;        // falsy
+
+  },
+
+};
+
